415ce8339ea360753787e72c3e901dbf4e396665
valbeat <kj1ktk@gmail.com>
2016-10-26 01:36:52 +0900

    create BubbleSort

--- modules/sort.ex
+++ modules/sort.ex
@@ -1,22 +1,16 @@
-defmodule Insertion do
-  def sort(list) when is_list(list) do
-    sort(list,[])
-  end
-  defp sort([],sorted) do
-    sorted
-  end
-  defp sort([h|t],sorted) do
-    sort(t, insert(h,sorted))
-  end
-  defp insert(x,[]) do
-    [x] 
-  end
-  defp insert(x,sorted) do
-    [h|t] = sorted
-    if x < h do
-      [x|sorted]
-    else
-      [h|insert(x,t)]
-    end
-  end
+defmodule InsertionSort do
+  def sort(list) when is_list(list), do: sort(list,[])
+  def sort([],sorted), do: sorted
+  def sort([h|t],sorted), do: sort(t, insert(h,sorted))
+  defp insert(x,[]), do: [x] 
+  defp insert(x,[h|t]) when x < h , do: [x] ++ [h|t]
+  defp insert(x,[h|t]), do: h ++ insert(x,t)
+end
+
+defmodule BubbleSort do
+  def sort(list) when is_list(list), do: sort(list, [], :pass)
+  def sort([x|[y|t]], acc, _status) when x > y ,do: sort([x] ++ t, acc ++ [y], :swap)
+  def sort([h|t],acc,status) ,do: sort(t, acc ++ [h],status)
+  def sort([],acc,:swap) ,do: sort(acc,[],:pass)
+  def sort([],acc,:pass) ,do: acc
 end
